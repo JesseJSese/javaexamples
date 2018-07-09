f6f339d461514ecae13446a0f940fe29268a0929
JesseJSese <40901720+JesseJSese@users.noreply.github.com>
2018-07-09 17:06:15 -0500

    Update Gradebook.java
    Fixed

--- Gradebook.java
+++ Gradebook.java
@@ -1,6 +1,6 @@
 public class GradeBook{
   public static void main(String[] args){
-  int grade = 82;
+  int grade = 5;
 /* A is a 90-100
    B is a 80-89
    C is a 70-79
@@ -8,11 +8,24 @@ public class GradeBook{
    F is less than 60
 */
     //adjust conditionals and print statements to fit the grading scale
-    if (grade >= 0 && grade < 0) {
+    if (grade >= 90 && grade < 100) {
       System.out.println("Your grade is an A");
     }
-    if (grade >= 0 && grade < 0) {
+    
+    if (grade >= 80 && grade < 89) {
       System.out.println("Your grade is a B");
+    }
+    
+     if (grade >= 70 && grade < 79) {
+      System.out.println("Your grade is a C");
+    }
+    
+     if (grade >= 60 && grade < 69) {
+      System.out.println("Your grade is a D");
+    }
+    
+     if (grade >= 0 && grade < 59) {
+      System.out.println("Your grade is a F");
     }
     // keep adding if statements to acoount for all the grade options
 
